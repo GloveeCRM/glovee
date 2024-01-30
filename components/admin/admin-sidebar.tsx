90cbf1fbfd267fcb59b76e29b238f385f1e99e4f
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-01-30 13:11:21 -0500

    Removed side bar on edit and preview pages

--- components/admin/admin-sidebar.tsx
+++ components/admin/admin-sidebar.tsx
@@ -1,6 +1,18 @@
+'use client'
+
 import NavLinks from './nav-links'
+import { usePathname } from 'next/navigation'
 
 export default function AdminSidebar() {
+  const pathname = usePathname()
+
+  const hideSidebarRegex = /\/admin\/templates\/[^\/]+\/(preview|edit)$/
+  const isSidebarHidden = hideSidebarRegex.test(pathname)
+
+  if (isSidebarHidden) {
+    return null
+  }
+
   return (
     <div id="adminSidebar" className="sticky top-0 flex h-screen w-[180px] flex-col bg-gray-500">
       <div className="bg-yellow-500">
