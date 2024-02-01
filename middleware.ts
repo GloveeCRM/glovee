422cee548a6f4d946d59032c84341a1261a22255
Ben Cyrus <bencyrus@Bens-MacBook-Air.local>
2024-01-31 21:18:21 -0500

    cleaned up middleware logic

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 
 import authConfig from '@/auth.config'
-import { DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, publicRoutes } from '@/routes'
+import { DEFAULT_LOGIN_REDIRECT, authRoutes, publicRoutes } from '@/routes'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(request: NextRequest) {
@@ -13,23 +13,13 @@ export async function middleware(request: NextRequest) {
   let response = NextResponse.next()
   response.headers.set('pathname', nextUrl.pathname)
 
-  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
   const isAuthRoute = authRoutes.includes(nextUrl.pathname)
 
-  if (isApiAuthRoute) {
-    return response
-  }
-
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      response = NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
-    }
-    return response
-  }
-
-  if (!isLoggedIn && !isPublicRoute) {
-    response = NextResponse.redirect(new URL('/login', nextUrl))
+  if (isAuthRoute && isLoggedIn) {
+    return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+  } else if (!isLoggedIn && !isPublicRoute && !isAuthRoute) {
+    return NextResponse.redirect(new URL('/login', nextUrl))
   }
 
   return response
