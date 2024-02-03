be8af0ce16b97efbd2c8371b4f6cd5fb8b748233
Ben Cyrus <mahdi.mohaghegh2001@gmail.com>
2024-02-03 14:13:36 -0500

    updated resend to work with environment variables

--- lib/mail/mail.ts
+++ lib/mail/mail.ts
@@ -3,10 +3,10 @@ import { Resend } from 'resend'
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `http://localhost:3000/new-verification?token=${token}`
+  const confirmLink = `${process.env.HOST_URL}/new-verification?token=${token}`
 
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: `${process.env.RESEND_VERIFICATION_FROM_EMAIL}`,
     to: email,
     subject: 'Confirm your email',
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email </p>`,
