fd91aecacb2f41eb7fdf3982fe07629a1d86dbf2
Ben Cyrus <mahdi.mohaghegh2001@gmail.com>
2024-02-03 13:30:32 -0500

    added privacy policy

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,14 @@ const nextConfig = {
       },
     ]
   },
+  async rewrites() {
+    return [
+      {
+        source: '/privacy-policy',
+        destination: '/html/privacy-policy.html',
+      },
+    ]
+  },
 }
 
 export default nextConfig
