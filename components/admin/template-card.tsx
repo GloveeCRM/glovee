d519ab814f20ea9fc241bddd99bb371419ad62e0
Ben Cyrus <mahdi.mohaghegh2001@gmail.com>
2024-02-04 01:47:30 -0500

    added transition

--- components/admin/template-card.tsx
+++ components/admin/template-card.tsx
@@ -20,20 +20,23 @@ export default function TemplateCard({
       <div className="mb-[8px] flex justify-between gap-[10px]">
         <p className="text-[16px] font-bold text-n-700">{title}</p>
         <Popover>
-          <PopoverTrigger className="rounded-[3px] transition" openClassName="bg-n-700 text-n-100">
+          <PopoverTrigger
+            className="rounded-[3px] transition duration-100"
+            openClassName="bg-n-700 text-n-100"
+          >
             <FiMoreHorizontal className="h-[22px] w-[22px]" />
           </PopoverTrigger>
           <PopoverContent
             position="bottom-left"
             className="mt-[3px] rounded-[3px] bg-n-700 p-[4px]
-             text-[14px] text-n-100 shadow-[0px_0px_0px_1px_rgba(15,15,15,0.05),0px_3px_6px_rgba(15,15,15,0.2),0px_9px_24px_rgba(15,15,15,0.2)] transition"
+             text-[14px] text-n-100 shadow-[0px_0px_0px_1px_rgba(15,15,15,0.05),0px_3px_6px_rgba(15,15,15,0.2),0px_9px_24px_rgba(15,15,15,0.2)] transition duration-100"
           >
             <ul>
               <li
                 onClick={() => {
                   console.log('delete')
                 }}
-                className="flex cursor-pointer items-center gap-[4px] rounded-sm p-[6px] font-medium text-red-500 hover:bg-n-600"
+                className="flex cursor-pointer items-center gap-[4px] rounded-sm p-[6px] font-medium text-red-500 transition duration-100 hover:bg-n-600"
               >
                 <span>
                   <BiTrash className="h-[18px] w-[18px]" />
