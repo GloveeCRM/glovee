a3bbcdb9a6595eee15463cfb31c4fc96b3dcc622
bencyrus <mahdi.mohaghegh2001@gmail.com>
2024-02-13 12:10:57 -0500

    Add subdomain support for verification email link

--- lib/mail/mail.ts
+++ lib/mail/mail.ts
@@ -1,9 +1,22 @@
+import { headers } from 'next/headers'
 import { Resend } from 'resend'
+import { extractSubdomainFromHostname } from '../utils/url'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `${process.env.NEXT_PUBLIC_ROOT_URL}/new-verification?token=${token}`
+  const headerList = headers()
+  const hostname = headerList.get('host')
+  const subdomain = extractSubdomainFromHostname(hostname!) || ''
+
+  let confirmLink = ''
+  if (!subdomain) {
+    confirmLink = `${process.env.NEXT_PUBLIC_ROOT_URL}/new-verification?token=${token}`
+  } else if (subdomain && process.env.NEXT_PUBLIC_ROOT_DOMAIN === 'localhost:3000') {
+    confirmLink = `http://${subdomain}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}/new-verification?token=${token}&subdomain=${subdomain}`
+  } else {
+    confirmLink = `https://${subdomain}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}/new-verification?token=${token}&subdomain=${subdomain}`
+  }
 
   await resend.emails.send({
     from: `${process.env.RESEND_VERIFICATION_FROM_EMAIL}`,
