057964f90b1eb9767af2ec2e50058a21cf8edcac
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-02-16 17:54:07 -0500

    Added background color to selected section in client sidebar

--- components/forms/questionSetType/flat-question-set.tsx
+++ components/forms/questionSetType/flat-question-set.tsx
@@ -2,7 +2,6 @@ import { QuestionSet } from '@/app/[domain]/(client)/applications/[id]/page'
 import TextInputQuestion from '../questionType/text-input-question'
 
 export default function FlatQuestionSet({ questionSet }: { questionSet: QuestionSet }) {
-  console.log(questionSet)
   return (
     <div className="m-[8px] bg-green-500 p-[8px]">
       {questionSet?.questions?.map((question) => (
