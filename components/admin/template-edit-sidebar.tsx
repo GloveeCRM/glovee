4efdea9a091738703eb4e81988ee03ee6c1c4794
bencyrus <mahdi.mohaghegh2001@gmail.com>
2024-02-18 17:59:09 -0500

    Refactor template edit sidebar

--- components/admin/template-edit-sidebar.tsx
+++ components/admin/template-edit-sidebar.tsx
@@ -6,12 +6,10 @@ import SidebarCategories from './sidebar-categories'
 export default async function TemplateEditSidebar({ templateId }: { templateId: string }) {
   return (
     <div className="sticky top-0 flex h-screen w-[240px] flex-col bg-n-700 text-white">
-      <div className="flex items-center gap-[4px] p-[8px]">
+      <Link href={`/admin/templates/`} className="flex w-fit items-center gap-[4px] p-[8px]">
         <IoChevronBackOutline className="h-[20px] w-[20px]" />
-        <Link href={`/admin/templates/`}>
-          <span className="text-[16px]">Back</span>
-        </Link>
-      </div>
+        <span className="text-[16px]">Back</span>
+      </Link>
       <TemplateInfoCard templateId={templateId} className="mx-[8px]" />
       <SidebarCategories templateId={templateId} />
     </div>
