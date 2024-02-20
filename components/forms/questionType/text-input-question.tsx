0148585ccffb80d6f18b48865f6ffdf03a265d71
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-02-20 12:21:03 -0500

    Add useDebounce hook to TextInputQuestion component

--- components/forms/questionType/text-input-question.tsx
+++ components/forms/questionType/text-input-question.tsx
@@ -1,8 +1,19 @@
 'use client'
 
 import { Question } from '@/app/[domain]/(client)/applications/[id]/page'
+import { useDebounce } from '@/hooks/use-debounce'
 import { saveInput } from '@/lib/actions/form'
+import { use, useEffect, useState } from 'react'
 export default function TextInputQuestion({ question }: { question: Question }) {
+  const [inputValue, setInputValue] = useState('')
+  const debouncedInput = useDebounce(inputValue, 1000)
+
+  useEffect(() => {
+    if (debouncedInput) {
+      saveInput({ input: debouncedInput, questionId: question.id })
+    }
+  }, [debouncedInput, question.id])
+
   return (
     <div>
       <p>{question.helperText}</p>
@@ -12,9 +23,7 @@ export default function TextInputQuestion({ question }: { question: Question }) 
         id="text-question"
         type="text"
         className="border border-black"
-        onChange={(e) => {
-          saveInput({ input: e.target.value, questionId: question.id })
-        }}
+        onChange={(e) => setInputValue(e.target.value)}
       />
     </div>
   )
