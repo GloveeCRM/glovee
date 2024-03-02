6888e4c344afd1406e81b0ae051e03bbdeaf350a
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-03-01 23:24:08 -0500

    Refactor fetching templates by user ID

--- components/admin/dashboard/templates/template-card-wrapper.tsx
+++ components/admin/dashboard/templates/template-card-wrapper.tsx
@@ -5,7 +5,8 @@ import TemplateCard from './template-card'
 
 export default async function TemplateCardWrapper() {
   const user = await getAuthenticatedUser()
-  const templates = await fetchTemplatesByUserId(user?.id!)
+  if (!user || !user.id) return null
+  const templates = await fetchTemplatesByUserId(user.id)
 
   return (
     <div className="grid grid-cols-1 gap-[8px] md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
