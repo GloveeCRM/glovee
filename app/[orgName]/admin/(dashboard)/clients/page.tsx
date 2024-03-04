fc61e2ff6f1a557e97cd7ac6b6e742c4cb204dec
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-03-04 16:46:44 -0500

    Update ClientsPage and fetchClientsByOrgName function

--- app/[orgName]/admin/(dashboard)/clients/page.tsx
+++ app/[orgName]/admin/(dashboard)/clients/page.tsx
@@ -1,7 +1,15 @@
-export default function ClientsPage() {
+import { fetchClientsByOrgName } from '@/lib/data/clients'
+
+export default async function ClientsPage({ params }: { params: { orgName: string } }) {
+  const orgName = params.orgName
+  const clients = await fetchClientsByOrgName(orgName)
   return (
     <div>
-      <h1 className="mb-[15px] text-[24px] font-bold">Clients</h1>
+      <h1 className="mb-[15px] text-[24px] font-bold">
+        {clients.map((client) => (
+          <div key={client.id}>{client.email}</div>
+        ))}
+      </h1>
     </div>
   )
 }
