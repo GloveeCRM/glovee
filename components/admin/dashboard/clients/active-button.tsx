e1882487339dc086f25a02e6031680531997e024
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-03-05 15:53:44 -0500

    Add activateClientById function to activateButton component

--- components/admin/dashboard/clients/active-button.tsx
+++ components/admin/dashboard/clients/active-button.tsx
@@ -1,6 +1,16 @@
-export default function ActiveButton() {
+'use client'
+
+import { activateClientById } from '@/lib/data/clients'
+
+export default function ActiveButton({ CLientId }: { CLientId: string }) {
+  async function handleClickReactivate() {
+    await activateClientById(CLientId)
+  }
   return (
-    <button className="rounded-md border-2 border-green-400 px-4 py-2 text-green-400">
+    <button
+      onClick={handleClickReactivate}
+      className="rounded-md border-2 border-green-400 px-4 py-2 text-green-400"
+    >
       Reactivate
     </button>
   )
