ece5b9a601ef6b8f25fc156750baedd989f22fcd
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-03-12 13:13:07 -0400

    Refactor application page layout and add new application card

--- components/admin/dashboard/applications/applications-page-toolbar.tsx
+++ components/admin/dashboard/applications/applications-page-toolbar.tsx
@@ -1,9 +1,11 @@
 import ApplicationsSearch from './applications-search'
+import CreateNewApplicationCard from './create-new-application-card'
 
 export default function ApplicationPageToolbar() {
   return (
-    <div>
+    <div className="flex items-end justify-between">
       <ApplicationsSearch placeholder="Search Applications" className="w-1/3" />
+      <CreateNewApplicationCard />
     </div>
   )
 }
