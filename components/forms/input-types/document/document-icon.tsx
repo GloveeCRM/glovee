9a6b6add84ebec27c2d84642c618d2dcf93ada97
bencyrus <mahdi.mohaghegh2001@gmail.com>
2024-03-22 01:00:35 -0400

    updated document logo

--- components/forms/input-types/document/document-icon.tsx
+++ components/forms/input-types/document/document-icon.tsx
@@ -4,17 +4,16 @@ import { PiCheckBold } from 'react-icons/pi'
 export default function DocumentIcon() {
   return (
     <div className="skew-x-0 rounded bg-n-200 px-[6px] py-[4px] text-[12px] text-n-600" draggable>
-      <span className="text-[12px]">Checkbox</span>
+      <span className="text-[12px]">Document</span>
       <div className="mb-[2px] mt-[4px] flex flex-col gap-[2px]">
-        <div className="flex w-[95px] items-center">
-          <div className="h-[12px] w-[12px] flex-shrink-0 rounded-sm border-[1px] border-n-600" />
-          <Divider className="ml-[5px] border-n-500" size={2} />
+        <div className="flex w-[35px] items-center">
+          <Divider className="mb-[2px] border-n-500" size={2} />
         </div>
-        <div className="flex w-[75px] items-center">
-          <div className="flex h-[12px] w-[12px] flex-shrink-0 items-center justify-center rounded-sm border-[1px] border-n-600 bg-n-600">
-            <PiCheckBold className="h-[10px] w-[10px] text-n-200" />
+        <div className="flex h-[20px] items-center rounded-sm border-[1px] border-n-600">
+          <div className="ml-[2px] h-[14px] w-[14px] bg-n-400" />
+          <div className="ml-[5px] w-[20px]">
+            <Divider className="border-n-500" size={2} />
           </div>
-          <Divider className="ml-[5px] border-n-500" size={2} />
         </div>
       </div>
     </div>
