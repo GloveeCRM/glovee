53378059d740b59406c3bc12d0d34477125ef61c
bencyrus <mahdi.mohaghegh2001@gmail.com>
2024-03-21 22:45:44 -0400

    updated textarea icon

--- components/forms/input-types/textarea/textarea-icon.tsx
+++ components/forms/input-types/textarea/textarea-icon.tsx
@@ -1,20 +1,31 @@
 import Divider from '@/components/ui/divider'
-import { PiCheckBold } from 'react-icons/pi'
 
 export default function TextareaIcon() {
   return (
     <div className="skew-x-0 rounded bg-n-200 px-[6px] py-[4px] text-[12px] text-n-600" draggable>
-      <span className="text-[12px]">Checkbox</span>
+      <span className="text-[12px]">Textarea</span>
       <div className="mb-[2px] mt-[4px] flex flex-col gap-[2px]">
-        <div className="flex w-[95px] items-center">
-          <div className="h-[12px] w-[12px] flex-shrink-0 rounded-sm border-[1px] border-n-600" />
-          <Divider className="ml-[5px] border-n-500" size={2} />
+        <div className="flex w-[35px] items-center">
+          <Divider className="border-n-500" size={2} />
         </div>
-        <div className="flex w-[75px] items-center">
-          <div className="flex h-[12px] w-[12px] flex-shrink-0 items-center justify-center rounded-sm border-[1px] border-n-600 bg-n-600">
-            <PiCheckBold className="h-[10px] w-[10px] text-n-200" />
+        <div className="flex h-[22px] rounded-sm border-[1px] border-n-600">
+          <div className="mt-[2px] flex">
+            <div className="ml-[4px] w-[15px]">
+              <Divider className="border-n-500" size={2} />
+            </div>
+            <div className="ml-[1px] w-[8px]">
+              <Divider className="border-n-500" size={2} />
+            </div>
+            <div className="ml-[1px] w-[5px]">
+              <Divider className="border-n-500" size={2} />
+            </div>
+            <div className="ml-[1px] w-[5px]">
+              <Divider className="border-n-500" size={2} />
+            </div>
+            <div className="ml-[1px] w-[5px]">
+              <Divider className="border-n-500" size={2} />
+            </div>
           </div>
-          <Divider className="ml-[5px] border-n-500" size={2} />
         </div>
       </div>
     </div>
