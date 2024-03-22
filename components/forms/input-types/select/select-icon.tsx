e91dbd09e0d4df754e4fdfc82b23273d8c2c9932
bencyrus <mahdi.mohaghegh2001@gmail.com>
2024-03-22 00:38:40 -0400

    updated select icon

--- components/forms/input-types/select/select-icon.tsx
+++ components/forms/input-types/select/select-icon.tsx
@@ -1,20 +1,21 @@
 import Divider from '@/components/ui/divider'
-import { PiCheckBold } from 'react-icons/pi'
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
 
 export default function SelectIcon() {
   return (
     <div className="skew-x-0 rounded bg-n-200 px-[6px] py-[4px] text-[12px] text-n-600" draggable>
-      <span className="text-[12px]">Checkbox</span>
+      <span className="text-[12px]">Select</span>
       <div className="mb-[2px] mt-[4px] flex flex-col gap-[2px]">
-        <div className="flex w-[95px] items-center">
-          <div className="h-[12px] w-[12px] flex-shrink-0 rounded-sm border-[1px] border-n-600" />
-          <Divider className="ml-[5px] border-n-500" size={2} />
+        <div className="flex w-[35px] items-center">
+          <Divider className="mb-[2px] border-n-500" size={2} />
         </div>
-        <div className="flex w-[75px] items-center">
-          <div className="flex h-[12px] w-[12px] flex-shrink-0 items-center justify-center rounded-sm border-[1px] border-n-600 bg-n-600">
-            <PiCheckBold className="h-[10px] w-[10px] text-n-200" />
+        <div className="flex h-[20px] items-center justify-between rounded-sm border-[1px] border-n-600">
+          <div className="ml-[5px] w-[20px]">
+            <Divider className="border-n-500" size={2} />
           </div>
-          <Divider className="ml-[5px] border-n-500" size={2} />
+          <span>
+            <MdOutlineKeyboardArrowDown className="h-[20px] w-[20px]" />
+          </span>
         </div>
       </div>
     </div>
