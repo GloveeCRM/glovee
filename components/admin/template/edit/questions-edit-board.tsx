9c104c86a27de6e4bfb24c3fcd184f6c59cda6de
bencyrus <mahdi.mohaghegh2001@gmail.com>
2024-03-29 17:37:40 -0400

    added empty question set dropzone

--- components/admin/template/edit/questions-edit-board.tsx
+++ components/admin/template/edit/questions-edit-board.tsx
@@ -1,3 +1,23 @@
+'use client'
+
+import { useTemplateEditContext } from '@/contexts/template-edit-context'
+import EmptySectionQuestionSetDropzone from './empty-section-question-set-dropzone'
+
 export default function QuestionsEditBoard() {
-  return <div id="questions-edit-board" className="h-[100px] rounded-lg bg-n-100 p-[4px]"></div>
+  const { template } = useTemplateEditContext()
+  const templateQuestionSets = template?.categories?.[0]?.sections?.[0]?.questionSets
+
+  return (
+    <div id="questions-edit-board" className="h-full rounded-lg bg-white p-[4px]">
+      {templateQuestionSets ? (
+        templateQuestionSets.map((questionSet) => (
+          <div key={questionSet.id} className="p-[4px]">
+            {questionSet.type}
+          </div>
+        ))
+      ) : (
+        <EmptySectionQuestionSetDropzone />
+      )}
+    </div>
+  )
 }
