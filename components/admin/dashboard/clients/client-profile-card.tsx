09ed138509b19d302b14228b1936c697773c5188
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-04-05 16:53:51 -0400

    Refactor client profile card component

--- components/admin/dashboard/clients/client-profile-card.tsx
+++ components/admin/dashboard/clients/client-profile-card.tsx
@@ -14,34 +14,38 @@ export default function ClientProfileCard({ client, userId }: { client: any; use
   const [isEditing, setIsEditing] = useState(false)
   return (
     <div className="flex items-center justify-between rounded-lg border border-n-400 bg-n-100/50 px-[14px] py-[18px]">
-      {isEditing ? (
-        <ClientProfileEdit setIsEditing={setIsEditing} client={client} />
-      ) : (
-        <div className="flex gap-[4px]">
-          <div className="flex items-center gap-[8px]">
-            <Image
-              src={client.image || DEFAULT_MALE_CLIENT_LOGO_URL}
-              alt="CLient Logo"
-              width={75}
-              height={75}
-              className="rounded-full"
-            />
-            <div className="text-[16px]">
-              <p>{client.name}</p>
-              <p>{client.email}</p>
-              <p>
-                <span className="rounded-full border bg-n-200 px-[6px] py-[2px] text-[12px]">
-                  {client.id}
-                </span>
-              </p>
-            </div>
-          </div>
-          <HiOutlinePencilSquare
-            className="h-[20px] w-[20px]"
-            onClick={() => setIsEditing(!isEditing)}
+      <div className="flex gap-[8px]">
+        <div>
+          <Image
+            src={client.image || DEFAULT_MALE_CLIENT_LOGO_URL}
+            alt="CLient Logo"
+            width={75}
+            height={75}
+            className="block rounded-full"
           />
         </div>
-      )}
+        {isEditing ? (
+          <ClientProfileEdit setIsEditing={setIsEditing} client={client} />
+        ) : (
+          <div className="flex gap-[4px]">
+            <div className="flex items-center gap-[8px]">
+              <div className="text-[16px]">
+                <p>{client.name}</p>
+                <p>{client.email}</p>
+                <p>
+                  <span className="rounded-full border bg-n-200 px-[6px] py-[2px] text-[12px]">
+                    {client.id}
+                  </span>
+                </p>
+              </div>
+            </div>
+            <HiOutlinePencilSquare
+              className="h-[20px] w-[20px]"
+              onClick={() => setIsEditing(!isEditing)}
+            />
+          </div>
+        )}
+      </div>
       <div>
         {(client.status === UserStatus.ACTIVE && <DeactiveButton CLientId={userId} />) ||
           (client.status === UserStatus.INACTIVE && <ActiveButton CLientId={userId} />)}
