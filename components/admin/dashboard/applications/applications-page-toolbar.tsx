09dd69a81e1380581ce8d5867dacddac5557f09f
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-04-06 22:13:08 -0400

    Refactor applications search component

--- components/admin/dashboard/applications/applications-page-toolbar.tsx
+++ components/admin/dashboard/applications/applications-page-toolbar.tsx
@@ -1,4 +1,4 @@
-import ApplicationsSearch from './applications-search'
+import SearchByParams from '@/components/search-by-params'
 import CreateNewApplicationButton from './create-new-application-button'
 
 interface ApplicationPageToolbarProps {
@@ -8,7 +8,7 @@ interface ApplicationPageToolbarProps {
 export default function ApplicationPageToolbar({ orgName }: ApplicationPageToolbarProps) {
   return (
     <div className="flex items-end justify-between">
-      <ApplicationsSearch placeholder="Search Applications" className="w-1/3" />
+      <SearchByParams placeholder="Search Applications" className="w-1/3" />
       <CreateNewApplicationButton orgName={orgName} />
     </div>
   )
