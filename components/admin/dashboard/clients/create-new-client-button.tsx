b495ee15204a2f30f61d9ed5b8b7537f53375b17
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-04-07 16:51:22 -0400

    Refactor CreateNewClientButton component

--- components/admin/dashboard/clients/create-new-client-button.tsx
+++ components/admin/dashboard/clients/create-new-client-button.tsx
@@ -1,17 +1,18 @@
-import { ModalProvider, ModalTrigger } from '@/components/ui/modal'
 import { GoPlus } from 'react-icons/go'
+
+import { ModalProvider, ModalTrigger } from '@/components/ui/modal'
 import CreateNewClientModal from './create-new-client-modal'
 
 export default function CreateNewClientButton() {
   return (
     <ModalProvider>
       <ModalTrigger>
-        <div className="flex cursor-pointer items-center gap-[4px] rounded border border-n-700 py-[5px] pl-[8px] pr-[10px]">
-          <GoPlus className="h-[20px] w-[20px]" />
+        <div className="group flex h-[36px] w-[130px] cursor-pointer items-center justify-center gap-[4px] rounded border border-n-600 text-n-600 hover:border-n-800 hover:font-medium hover:text-n-800">
+          <GoPlus className="h-[20px] w-[20px] text-n-600 group-hover:text-n-800" />
           <h1>New Client</h1>
-          <CreateNewClientModal />
         </div>
       </ModalTrigger>
+      <CreateNewClientModal />
     </ModalProvider>
   )
 }
