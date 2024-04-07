1469924c6c1bae5380cc6655f906c3057b2a712d
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-04-06 23:32:27 -0400

    Refactor search input styling

--- components/search-by-params.tsx
+++ components/search-by-params.tsx
@@ -16,15 +16,15 @@ export default function SearchByParams({ placeholder }: SearchByParamsProps) {
   }
 
   return (
-    <div className="relative flex flex-1 flex-shrink-0">
+    <div className="relative">
       <input
         type="text"
-        className="block h-[40px] w-full rounded border border-n-400 py-[9px] pl-[35px] text-[14px] placeholder:text-gray-500 focus:border-n-500 focus:outline-none"
+        className="peer h-[40px] w-full rounded border border-n-400 pl-[35px] text-[14px] placeholder:text-n-500 focus:border-n-600 focus:outline-none"
         onChange={handleChange}
         placeholder={placeholder}
         defaultValue={initialValue}
       />
-      <IoSearch className="absolute left-[10px] top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+      <IoSearch className="absolute left-[10px] top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-n-500 peer-focus:text-n-700" />
     </div>
   )
 }
