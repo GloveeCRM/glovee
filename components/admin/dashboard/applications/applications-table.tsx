56291b8ee76a85e5f9a2f084d5407e292940108c
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-04-16 11:34:04 -0400

    Refactor ApplicationsTable component and fetchApplicationByOrgNameandSearchQuery function

--- components/admin/dashboard/applications/applications-table.tsx
+++ components/admin/dashboard/applications/applications-table.tsx
@@ -26,37 +26,5 @@ export default async function ApplicationsTable({ orgName, query }: Applications
         />
       ))}
     </Table>
-
-    // <table className="mt-[28px] w-full text-[14px]">
-    //   <tbody>
-    //     <tr className="border-b-2 border-n-700 text-left">
-    //       <th>Application ID</th>
-    //       <th>Client</th>
-    //       <th>Template</th>
-    //       <th>Role</th>
-    //       <th>Status</th>
-    //     </tr>
-    //     {applications.length === 0 ? (
-    //       <tr>
-    //         <td colSpan={5} className="py-[12px] text-center text-n-500">
-    //           No applications found
-    //         </td>
-    //       </tr>
-    //     ) : (
-    //       applications.map((application) => (
-    //         <ApplicationsTableRow
-    //           id={application.id}
-    //           clientName={application.client.name!}
-    //           templateName={application.templateName}
-    //           role={application.role}
-    //           status={application.status}
-    //           key={application.id}
-    //           applicantFirstName={application.applicantFirstName}
-    //           applicantLastName={application.applicantLastName}
-    //         />
-    //       ))
-    //     )}
-    //   </tbody>
-    // </table>
   )
 }
