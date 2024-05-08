4561d0bf48219eb6761c3c820e48b68c4e5ee229
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-05-08 17:05:07 -0400

    Refactor application categories progress component

--- components/client/application-categories-progress.tsx
+++ components/client/application-categories-progress.tsx
@@ -1,7 +1,16 @@
 import { ApplicationCategoryType } from '@/lib/types/application'
 import Divider from '../ui/divider'
 
-const percentages = [20, 20, 20, 20, 20, 20, 20, 20]
+const objects = [
+  { percentage: 10, category: 'Category 1' },
+  { percentage: 20, category: 'Category 2' },
+  { percentage: 30, category: 'Category 3' },
+  { percentage: 40, category: 'Category 4' },
+  { percentage: 50, category: 'Category 5' },
+  { percentage: 60, category: 'Category 6' },
+  { percentage: 70, category: 'Category 7' },
+  { percentage: 80, category: 'Category 8' },
+]
 
 export default function ApplicationCategoriesProgress({
   categories,
@@ -10,26 +19,27 @@ export default function ApplicationCategoriesProgress({
 }) {
   if (categories) {
     return (
-      <div className="flex flex-col items-center">
-        <div className="flex w-full justify-between">
-          {percentages.map((percentage, index) => (
-            <div key={index} className="w-full">
-              <div className="flex items-center text-[10px]">
-                <div className="rounded-full border border-n-300 px-[2px] py-[5px]">
-                  {percentage}%
-                </div>
-                {index < percentages.length - 1 && <Divider size={2} className="border-n-400" />}
+      <div className="flex justify-center">
+        {objects.map((object, index) => (
+          <div key={index} className="flex w-full flex-col items-center">
+            <div className="flex w-full items-center">
+              {index === 0 ? (
+                <Divider className="invisible" />
+              ) : (
+                <Divider size={2} className="border-n-400" />
+              )}
+              <div className="rounded-full border px-[2px] py-[5px] text-[10px]">
+                {object.percentage}%
               </div>
+              {index === objects.length - 1 ? (
+                <Divider className="invisible" />
+              ) : (
+                <Divider size={2} className="border-n-400" />
+              )}
             </div>
-          ))}
-        </div>
-        <div className="mt-1 flex">
-          {categories.map((category) => (
-            <div key={category.id} className="w-20 text-center text-[10px] text-n-700">
-              {category.title}
-            </div>
-          ))}
-        </div>
+            <div>{object.category}</div>
+          </div>
+        ))}
       </div>
     )
   } else {
