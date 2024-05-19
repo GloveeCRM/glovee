64759a68294476214b50835f96d70e755f8a8d9d
bencyrus <mahdi.mohaghegh2001@gmail.com>
2024-05-18 20:56:05 -0400

    added template searching functionality

--- lib/types/template.ts
+++ lib/types/template.ts
@@ -13,6 +13,13 @@ export type TemplateType = Template & {
   categories?: TemplateCategoryType[]
 }
 
+export type TemplateType2 = {
+  id: string
+  organizationID: string
+  name: string
+  description: string
+}
+
 export type TemplateCategoryType = TemplateCategory & {
   sections?: TemplateSectionType[]
 }
