28bb4ab64716fbfe4f3dad7b8510ba0501c1cbf2
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-05-19 22:45:51 -0400

    Add applicationQuestionSets json

--- app/[orgName]/(client)/application/[id]/page.tsx
+++ app/[orgName]/(client)/application/[id]/page.tsx
@@ -23,6 +23,83 @@ export interface Answer {
   questionId: string
 }
 
+const applicationQuestionSets = [
+  {
+    id: 'clwcpnpde0004hy7g3u064v77',
+    type: 'FLAT',
+    position: 0,
+    sectionId: 'clwcpnpde0003hy7g1t9qfaum',
+    questionSetId: null,
+    questions: [
+      {
+        id: 'clwcpnpde0005hy7gn753phvl',
+        type: 'TEXT_INPUT',
+        prompt: 'name',
+        position: 0,
+        helperText: 'No helper text',
+        settings: {},
+        questionSetId: 'clwcpnpde0004hy7g3u064v77',
+      },
+    ],
+  },
+  {
+    id: 'clwcpnpde0006hy7gqetldv4q',
+    type: 'LOOP',
+    position: 1,
+    sectionId: 'clwcpnpde0003hy7g1t9qfaum',
+    questionSetId: null,
+    questions: [],
+    questionSets: [
+      {
+        id: 'clwcpnpde0007hy7ghbzhedng',
+        type: 'LOOP',
+        position: 0,
+        sectionId: 'clwcpnpde0003hy7g1t9qfaum',
+        questionSetId: 'clwcpnpde0006hy7gqetldv4q',
+        questions: [],
+        questionSets: [
+          {
+            id: 'clwcpnpdf0008hy7ggzqdt9bf',
+            type: 'FLAT',
+            position: 0,
+            sectionId: 'clwcpnpde0003hy7g1t9qfaum',
+            questionSetId: 'clwcpnpde0007hy7ghbzhedng',
+            questions: [
+              {
+                id: 'clwcpnpdf0009hy7g2yg0effp',
+                type: 'TEXT_INPUT',
+                prompt: 'job title in the company',
+                position: 0,
+                helperText: 'No helper text',
+                settings: {},
+                questionSetId: 'clwcpnpdf0008hy7ggzqdt9bf',
+              },
+            ],
+          },
+          {
+            id: 'clwcpnpdf000ahy7g2fn3czm7',
+            type: 'FLAT',
+            position: 1,
+            sectionId: 'clwcpnpde0003hy7g1t9qfaum',
+            questionSetId: 'clwcpnpde0007hy7ghbzhedng',
+            questions: [
+              {
+                id: 'clwcpnpdf000bhy7g644o960a',
+                type: 'TEXT_INPUT',
+                prompt: 'company name',
+                position: 0,
+                helperText: 'No helper text',
+                settings: {},
+                questionSetId: 'clwcpnpde0006hy7gqetldv4q',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+] as ApplicationQuestionSetType[]
+
 export default async function ClientApplicationPage({
   searchParams,
 }: {
@@ -33,5 +110,5 @@ export default async function ClientApplicationPage({
     sectionId
   )) as ApplicationQuestionSetType[]
 
-  return <div>{JSON.stringify(questionSets)}</div>
+  return <div></div>
 }
