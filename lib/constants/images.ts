2d5ee83521ef62166fb46f24272f9d79ac2fe453
bencyrus <mahdi.mohaghegh2001@gmail.com>
2024-05-22 00:22:55 -0400

    added default picture

--- lib/constants/images.ts
+++ lib/constants/images.ts
@@ -6,4 +6,4 @@ export const DEFAULT_ORG_LOGO_URL = `https://${process.env.NEXT_PUBLIC_BLOB_HOST
 /**
  * URL for the default male client logo.
  */
-export const DEFAULT_MALE_CLIENT_LOGO_URL = `https://${process.env.NEXT_PUBLIC_BLOB_HOST}/client-profile-pictures/male-default-profile-picture.jpg`
+export const DEFAULT_MALE_CLIENT_LOGO_URL = `https://${process.env.NEXT_PUBLIC_BLOB_HOST}/client/profile-pictures/male-default-profile-picture.jpg`
