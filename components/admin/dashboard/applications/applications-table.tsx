238e9b782c5542513d56e4cd373e72226296bba4
bencyrus <mahdi.mohaghegh2001@gmail.com>
2024-05-30 10:20:55 -0400

    added room to the applications table for pagination

--- components/admin/dashboard/applications/applications-table.tsx
+++ components/admin/dashboard/applications/applications-table.tsx
@@ -22,53 +22,56 @@ export default async function ApplicationsTable({ orgName, query }: Applications
   const applications = await searchApplications(orgName, query, 50, 0)
 
   return (
-    <div className="mt-[20px] h-full overflow-auto">
-      <Table>
-        <TableHeader className="sticky top-0 bg-white shadow-sm">
-          <TableRow>
-            <TableHead>Application ID</TableHead>
-            <TableHead>Client</TableHead>
-            <TableHead>Template</TableHead>
-            <TableHead>Role</TableHead>
-            <TableHead>Status</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {applications.map((application) => (
-            <TableRow key={application.id}>
-              <TableCell>{application.id}</TableCell>
-              <TableCell>
-                <Link href={`/admin/clients/${application.clientID}`}>
-                  <div className="flex items-center gap-[6px]">
-                    <div>
-                      <Image
-                        src={DEFAULT_MALE_CLIENT_LOGO_URL}
-                        alt=""
-                        width={30}
-                        height={30}
-                        className="rounded-full"
-                      />
-                    </div>
-                    <div>{application.clientID}</div>
-                  </div>
-                </Link>
-              </TableCell>
-              <TableCell>{application.templateName}</TableCell>
-              <TableCell className="max-w-[100px] truncate">
-                <Badge variant="secondary" className="flex w-fit gap-[2px]">
-                  <span className="font-semibold">{application.role}</span>
-                  <span>
-                    ({application.applicantFirstName} {application.applicantLastName})
-                  </span>
-                </Badge>
-              </TableCell>
-              <TableCell>
-                <Badge>{application.status}</Badge>
-              </TableCell>
+    <div className="mt-[20px] flex h-full flex-col overflow-auto">
+      <div className="overflow-auto">
+        <Table>
+          <TableHeader className="sticky top-0 bg-white shadow-sm">
+            <TableRow>
+              <TableHead>Application ID</TableHead>
+              <TableHead>Client</TableHead>
+              <TableHead>Template</TableHead>
+              <TableHead>Role</TableHead>
+              <TableHead>Status</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {applications.map((application) => (
+              <TableRow key={application.id}>
+                <TableCell>{application.id}</TableCell>
+                <TableCell>
+                  <Link href={`/admin/clients/${application.clientID}`}>
+                    <div className="flex items-center gap-[6px]">
+                      <div>
+                        <Image
+                          src={DEFAULT_MALE_CLIENT_LOGO_URL}
+                          alt=""
+                          width={30}
+                          height={30}
+                          className="rounded-full"
+                        />
+                      </div>
+                      <div>{application.clientID}</div>
+                    </div>
+                  </Link>
+                </TableCell>
+                <TableCell>{application.templateName}</TableCell>
+                <TableCell className="max-w-[100px] truncate">
+                  <Badge variant="secondary" className="flex w-fit gap-[2px]">
+                    <span className="font-semibold">{application.role}</span>
+                    <span>
+                      ({application.applicantFirstName} {application.applicantLastName})
+                    </span>
+                  </Badge>
+                </TableCell>
+                <TableCell>
+                  <Badge>{application.status}</Badge>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="bg-red-100">Hi</div>
     </div>
   )
 }
