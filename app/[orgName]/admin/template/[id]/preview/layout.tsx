5a0972856eceda30533117eaf01b2a953872a4f2
bencyrus <mahdi.mohaghegh2001@gmail.com>
2024-06-04 23:34:36 -0400

    removed unnecessary props from template preview layout

--- app/[orgName]/admin/template/[id]/preview/layout.tsx
+++ app/[orgName]/admin/template/[id]/preview/layout.tsx
@@ -1,23 +1,13 @@
 import TemplatePreviewSidebar from '@/components/admin/template/preview/template-preview-sidebar'
 
 interface TemplatePreviewLayoutProps {
-  params: {
-    id: string
-    orgName: string
-  }
   children: React.ReactNode
 }
 
-export default async function TemplatePreviewLayout({
-  params,
-  children,
-}: TemplatePreviewLayoutProps) {
-  const templateID = parseInt(params.id)
-  const orgName = params.orgName
-
+export default async function TemplatePreviewLayout({ children }: TemplatePreviewLayoutProps) {
   return (
     <div id="templatePreviewLayout" className="flex">
-      <TemplatePreviewSidebar orgName={orgName} templateID={templateID} />
+      <TemplatePreviewSidebar />
       <div className="w-full p-[8px]">{children}</div>
     </div>
   )
