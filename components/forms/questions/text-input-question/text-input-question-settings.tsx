368a34d6ad404bd1fdafca7229ddaebe49198d5a
bencyrus <mahdi.mohaghegh2001@gmail.com>
2024-06-17 22:09:09 -0400

    added the ability to toggle required on and off

--- components/forms/questions/text-input-question/text-input-question-settings.tsx
+++ components/forms/questions/text-input-question/text-input-question-settings.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import { Switch } from '@/components/ui/switch'
+import useQuestionActions from '@/hooks/template/use-question-actions'
 import { TextInputQuestionType } from '@/lib/types/qusetion'
 
 interface TextInputQuestionSettingsProps {
@@ -6,12 +9,21 @@ interface TextInputQuestionSettingsProps {
 }
 
 export default function TextInputQuestionSettings({ question }: TextInputQuestionSettingsProps) {
+  const { updateQuestion } = useQuestionActions()
+
+  function handleChangeIsRequired(isChecked: boolean) {
+    updateQuestion({
+      ...question,
+      settings: { ...question.settings, isRequired: isChecked },
+    })
+  }
+
   return (
     <div>
       <div className="flex gap-[6px]">
         <div>isRequired</div>
         <div>{String(question.settings.isRequired)}</div>
-        <Switch checked={question.settings.isRequired} />
+        <Switch checked={question.settings.isRequired} onCheckedChange={handleChangeIsRequired} />
       </div>
       <div className="flex gap-[6px]">
         <div>Placeholder</div>
