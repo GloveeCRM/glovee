5d9f2a42a21ea7dfe04818ea7e15c06a23d6c2e9
bencyrus <mahdi.mohaghegh2001@gmail.com>
2024-11-16 11:57:39 -0500

    updated client applications page

--- components/client/client-applications-wrapper.tsx
+++ components/client/client-applications-wrapper.tsx
@@ -1,14 +1,14 @@
 import { getSessionUserID } from '@/lib/auth/session'
-import { searchApplicationsOld } from '@/lib/data/application'
+import { searchApplications } from '@/lib/data/application'
 import ClientApplicationSummaryCard from './application/client-application-summary-card'
 
 export default async function ClientApplicationsWrapper() {
   const userID = await getSessionUserID()
-  const { applications } = await searchApplicationsOld({
+  const { applications } = await searchApplications({
     filters: { userID },
   })
 
-  const hasApplications = applications && applications?.length > 0
+  const hasApplications = applications && applications.length > 0
 
   return !hasApplications ? (
     <div className="flex h-[calc(100svh-16px)] flex-1 flex-col items-center justify-center text-[20px] text-n-500">
